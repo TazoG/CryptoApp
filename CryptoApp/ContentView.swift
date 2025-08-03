fe118a6b7694d41fbb0ca6ff099b25e0a56fe553
Tazo <tazogigitashvili@gmail.com>
2025-08-03 11:56:38 +0400

    Initial commit: CryptoApp with SwiftUI setup, Let's go

---
+++ CryptoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoApp
+//
+//  Created by Tazo Gigitashvili on 03.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Tazo Gigitashvili on 03.08.25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
